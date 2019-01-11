8930ef501d82b8591db74961ffd001c76596a969
Fabian Keller <github.100.fkeller@spamgourmet.com>
2019-01-11 08:09:52 +0100

    minimal karax example

--- src/main.nim
+++ src/main.nim
@@ -1,12 +1,4 @@
-import jsffi except `&`
-
-#{.experimental: "notnil".}
-#import karax/prelude
-#import karax/kdom
-#import karax/vdom
-#import karax/karax
-include karax/prelude
-import karax_utils
+import jsffi
 
 {.emit: """
 const electron = require('electron')
@@ -28,57 +20,6 @@ let app = electron.app
 let browserWindow = electron.BrowserWindow
 
 var mainWindow: JsObject
-#var client = jsnew net.createConnection(JsObject{ port: 1234 }, proc () =
-#  console.log("Connected to server!")
-#)
-
-
-type
-  # Elm's `model`
-  Model = ref object
-    toggle: bool
-
-
-proc init(): Model =
-  # Elm's `init`
-  Model(toggle: true)
-
-
-proc onClick(model: Model, ev: Event, n: VNode) =
-  # Elm's `update`. Note that we could have
-  # a global `update` like Elm as well. In this case
-  # the event handlers would construct a message type
-  # and pass it to a model update function.
-  # See second ElmLike demo for an example
-  kout(ev)
-  kout(model)
-  model.toggle = model.toggle xor true
-
-
-proc view(model: Model): VNode =
-  # Elm's `view`.
-  result = buildHtml():
-    tdiv:
-      # message handler close over the model using the curry macro
-      button(onclick=curry(onClick, model)):
-        text "click me"
-      tdiv:
-        text "Toggle state:"
-      tdiv:
-        if model.toggle:
-          text "true"
-        else:
-          text "false"
-
-# Putting it all together
-proc runMain() =
-  var model = init()
-
-  proc renderer(): VNode =
-    view(model)
-
-  kout("Setting renderer...")
-  setRenderer(renderer, "ROOT")
 
 
 proc createWindow() =
@@ -92,11 +33,6 @@ proc createWindow() =
   mainWindow.on("closed", proc () =
     mainWindow = nil
   )
-
-  #client.write "Hello, world\r\n"
-
-  runMain()
-
 
 app.on("ready", createWindow)
 
---
+++ src/renderer.nim
@@ -0,0 +1,57 @@
+#{.experimental: "notnil".}
+#import karax/prelude
+#import karax/kdom
+#import karax/vdom
+#import karax/karax
+include karax/prelude
+import karax_utils
+
+type
+  # Elm's `model`
+  Model = ref object
+    toggle: bool
+
+
+proc init(): Model =
+  # Elm's `init`
+  Model(toggle: true)
+
+
+proc onClick(model: Model, ev: Event, n: VNode) =
+  # Elm's `update`. Note that we could have
+  # a global `update` like Elm as well. In this case
+  # the event handlers would construct a message type
+  # and pass it to a model update function.
+  # See second ElmLike demo for an example
+  kout(ev)
+  kout(model)
+  model.toggle = model.toggle xor true
+
+
+proc view(model: Model): VNode =
+  # Elm's `view`.
+  result = buildHtml():
+    tdiv:
+      # message handler close over the model using the curry macro
+      button(onclick=curry(onClick, model)):
+        text "click me"
+      tdiv:
+        text "Toggle state:"
+      tdiv:
+        if model.toggle:
+          text "true"
+        else:
+          text "false"
+
+# Putting it all together
+proc runMain() =
+  var model = init()
+
+  proc renderer(): VNode =
+    view(model)
+
+  kout("Setting renderer...")
+  setRenderer(renderer, "ROOT")
+
+
+runMain()
